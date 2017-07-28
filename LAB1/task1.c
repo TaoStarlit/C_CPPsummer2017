f4d0fd3c042096d562b040f8c92bf91638386936
tear <tear@macs-iMac.local>
2017-07-28 11:51:31 +0800

    LAB1 optional

--- LAB1/task1.c
+++ LAB1/task1.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
+/*one way to get a input string without newlines
+    char c;
+    while((c=fgetc(stdin)) != '\n')
+        buffer[i++] = c;
+    
+*/
+
 #define INPUT_LEN 80
+
+
+void myfgets(char * __restrict buffer, int length, FILE* file)
+{
+    char c;
+    int i=0;
+    while(((c=fgetc(file)) != '\n') && i<length)
+        buffer[i++] = c;
+    buffer[i++]='\0';
+}
 
 int main()
 {
@@ -9,11 +26,13 @@ int main()
 
     //printf("task1 says:hello world!\r\n");
 
-    //printf("Please input your name:");
-    fgets(name, INPUT_LEN, stdin);
-    //printf("Please input your studentID:");
-    fgets(stuID, INPUT_LEN, stdin);
+    printf("Please input your name:");
+    myfgets(name, INPUT_LEN, stdin);
+    printf("Please input your studentID:");
+    myfgets(stuID, INPUT_LEN, stdin);
 
-    printf("Welcome	%s id:%s to the C and C++ world\r\n", name, stuID);//output of name and ID have \n, what is the reason
+
+
+    printf("Welcome %s id:%s to the C and C++ world\r\n", name, stuID);//output of name and ID have \n, what is the reason
     return 0;
 }
--- LAB1/task3.c
+++ LAB1/task3.c
@@ -2,100 +2,161 @@
 #include <string.h>
 #include <ctype.h>
 
-#define INPUT_LEN 80
+#define INPUT_LEN 100
 
 char alphabet[26][26];
-char *key="Vigenere";
+char *key = "Vigenere"; //;"aaaaaa"
 char upperkey[9];
 
 void InitShiftAlphabet(char bet[26][26])
 {
     char *p;
     //test
-    char i=0;
-    char j=0;
-    for(i=0;i<26;i++)   
+    char i = 0;
+    char j = 0;
+    for (i = 0; i < 26; i++)
     {
-        p=bet[i];
-        for(j=0;j<26;j++)
+        p = bet[i];
+        for (j = 0; j < 26; j++)
         {
             //printf("%c",(char)('A'+(i+j)%26));
-            *(p+j)=(char)('A'+(i+j)%26);
+            *(p + j) = (char)('A' + (i + j) % 26);
         }
     }
 }
 
-void PrintAlphabet(char bet[26][26])//char **bet or *bet[26] if Segmentation fault: 11   because **bet is a pointer to a pointer, it doesn't know the length is 26
+void PrintAlphabet(char bet[26][26]) //char **bet or *bet[26] if Segmentation fault: 11   because **bet is a pointer to a pointer, it doesn't know the length is 26
 {
-    char i,j;
+    char i, j;
     printf("\r\nPrintAlphabet:\r\n");
-    for(i=0;i<26;i++)   
+    for (i = 0; i < 26; i++)
     {
-        for(j=0;j<26;j++)
+        for (j = 0; j < 26; j++)
         {
-            printf("%c",bet[i][j]);
-        }   
+            printf("%c", bet[i][j]);
+        }
         printf("\n");
     }
 }
 
-void VigenereEndoder(char *s, char *d)
+void VigenereEncoder(char *s, char *d)
 {
     char *k = upperkey;
-    do{
-        *d=toupper(*s);
-        if ((*d>='A')&&(*d<='Z'))
+    do
+    {
+        *d = toupper(*s);
+        if ((*d >= 'A') && (*d <= 'Z'))
         {
-            *d=alphabet[*d-'A'][*k-'A'];
-            if (*(++k)=='\0') 
+            *d = alphabet[*d - 'A'][*k - 'A'];
+            if (*(++k) == '\0')
             {
-                k=upperkey;
+                k = upperkey;
             }
             //printf("*k:%c\r\n",*k);
         }
         //printf("s:%s, d:%s\r\n",s,d);
-        
+
         d++;
-    }while(*++s);
-    *d='\0';
+    } while (*++s);
+    *d = '\0';
 }
 
-int main(int argc,char *argv[])//argc==1, means no parameter, and only the ./prog
+void VigenereEncoderWithoutBet(char *s, char *d)
 {
-    char testcode[INPUT_LEN]="";
-    char decoderString[INPUT_LEN]="";
+    char *k = upperkey;
+    do
+    {
+        *d = toupper(*s);
+        if ((*d >= 'A') && (*d <= 'Z'))
+        {
+            //*d=alphabet[*d-'A'][*k-'A'];//shift forward *d-'A', shift forward '*k-A'
+            //*d += *d - 'A' + (*k - 'A');//wrong; because  *d-'A' means select the rows of the bet, so the encoded chat should be 'A'+ *d-'A' = *d
+            *d += (*k - 'A');
+            if (*d > 'Z')
+                *d -= 26;
+            if (*(++k) == '\0')
+            {
+                k = upperkey;
+            }
+            //printf("*k:%c\r\n",*k);
+        }
+        //printf("s:%s, d:%s\r\n",s,d);
+
+        d++;
+    } while (*++s);
+    *d = '\0';
+}
+
+void VigenereDecoderWithoutBet(char *s, char *d)
+{
+    char *k = upperkey;
+    do
+    {
+        *d = toupper(*s);
+        if ((*d >= 'A') && (*d <= 'Z'))
+        {
+            //*d=alphabet[*d-'A'][*k-'A'];//shift forward *d-'A', shift forward '*k-A'
+            //*d += *d - 'A' + (*k - 'A');//wrong; because  *d-'A' means select the rows of the bet, so the encoded chat should be 'A'+ *d-'A' = *d
+            *d -= (*k - 'A');
+            if (*d < 'A')
+                *d += 26;
+            if (*(++k) == '\0')
+            {
+                k = upperkey;
+            }
+            //printf("*k:%c\r\n",*k);
+        }
+        //printf("s:%s, d:%s\r\n",s,d);
+
+        d++;
+    } while (*++s);
+    *d = '\0';
+}
+
+int main(int argc, char *argv[]) //argc==1, means no parameter, and only the ./prog
+{
+    char testString[INPUT_LEN] = "";
+    char encoderString[INPUT_LEN] = "";
+    char encoderStringWithouBet[INPUT_LEN] = "";
+    char decoderStringWithouBet[INPUT_LEN] = "";
     char i;
 
-    
-    printf("argc:%d\r\n",argc);
-    if(argc==1)
-        strcpy(testcode,"task3 says: Hello world");
-    if(argc==2)
+    printf("argc:%d\r\n", argc);
+    if (argc == 1)
+        strcpy(testString, "task3 says: Hello world");
+    if (argc == 2)
     {
-        if(strcmp("pipe",argv[1])==0)
-            fgets(testcode,INPUT_LEN,stdin);
+        if (strcmp("pipe", argv[1]) == 0)
+        {
+            printf("using unix pipe ...");
+            fgets(testString, INPUT_LEN, stdin);
+        }
         else
-            strcpy(testcode,argv[1]);
+            strcpy(testString, argv[1]);
     }
 
-    printf("\r\nlength of key %s: %ld\r\n", key, strlen(key)); //strlen doesn't count the '\0' in, though it is exist in the stack memory  
-    printf("\r\nlength of testString %s: %ld\r\n", testcode, strlen(testcode));
-    for(i=0;i<8;i++)
+    printf("\r\nlength of key %s: %ld\r\n", key, strlen(key)); //strlen doesn't count the '\0' in, though it is exist in the stack memory
+    printf("\r\nlength of testString %s: %ld\r\n\r\n", testString, strlen(testString));
+    for (i = 0; i < 8; i++)
     {
-        upperkey[i]=toupper(key[i]);
+        upperkey[i] = toupper(key[i]);
         //printf("%c",upperkey[i]);
     }
-    upperkey[i]='\0';
-    printf("upperkey:%s\r\n", upperkey);
-    printf("testcode:%s\r\n", testcode);
-
+    upperkey[i] = '\0';
+    //printf("upperkey:%s\r\n", upperkey);
+    //printf("testString:%s\r\n", testString);
 
     InitShiftAlphabet(alphabet);
     //PrintAlphabet(alphabet);
 
-    VigenereEndoder(testcode, (char*)decoderString);
+    VigenereEncoder(testString, (char *)encoderString);
+    VigenereEncoderWithoutBet(testString, (char *)encoderStringWithouBet);
+    VigenereDecoderWithoutBet(encoderString, (char *)decoderStringWithouBet);
 
-    printf("decoderString: %s \r\n\r\n", decoderString);
+    printf("testString:\t\t %s \r\n", testString);
+    printf("encoderString:\t\t %s \r\n", encoderString);
+    printf("encoderStringWithouBet:\t %s \r\n", encoderStringWithouBet);
+    printf("decoderStringWithouBet:\t %s \r\n", decoderStringWithouBet);
 
     return 0;
 }
